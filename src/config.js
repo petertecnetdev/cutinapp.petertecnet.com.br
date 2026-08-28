d60f6e41ad5e9262a7791d61d290d0f5c8b864a3
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 20:15:19 -0300

    Prepare Cutinapp for app-scoped API v1

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 const apiBaseUrl = "https://api.petertecnet.com.br/api";
 const storageUrl = "https://api.petertecnet.com.br/storage/app/public/";
-export { apiBaseUrl, storageUrl };
+const appSlug = "cutinapp";
+const apiV1BaseUrl = `${apiBaseUrl}/v1/apps/${appSlug}`;
+
+export { apiBaseUrl, apiV1BaseUrl, storageUrl, appSlug };
