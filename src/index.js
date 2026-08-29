49e1a6f9bef67a86a370ee03513a0e204e068bcd
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:44:52 -0300

    Sign every Cutinapp axios request

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"; 
+import axios from "axios";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+axios.defaults.headers.common["X-Peter-App"] = "cutinapp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
