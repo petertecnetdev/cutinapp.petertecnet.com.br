49cc74f51d65d654b67510ce63bcdefe995c0917
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 20:52:54 -0300

    style: load complete Peter Tecnet layout system

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
+import "./App.css";
+import "./pages/LegacyPages.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
