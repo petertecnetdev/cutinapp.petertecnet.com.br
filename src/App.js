bb13e409848fc393d23163397c898ae5ef7ef83e
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 21:34:44 -0300

    feat(cutinapp): conecta compra gestao e check-in

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ import EmailVerifyPage from "./pages/auth/EmailVerifyPage";
 import LogoutPage from "./pages/auth/LogoutPage";
 import PasswordEmailPage from "./pages/auth/PasswordEmailPage";
 import PasswordPage from "./pages/auth/PasswordPage";
-import DashboardPage from "./pages/DashboardPage";
 import LoadingComponent from "./components/LoadingComponent";
 import authService from "./services/AuthService";
 
 import CutinHomePage from "./pages/CutinHomePage";
-import {
-  EventsHubPage,
-  ProducerDashboardPage,
-  TicketsPage,
-  MarketplacePage,
-  TeamPage,
-} from "./pages/CutinWorkspacePages";
+import { EventsHubPage, ProducerDashboardPage, TicketsPage, MarketplacePage, TeamPage } from "./pages/CutinWorkspacePages";
+import { PublicEventPage, EventManagePage, CheckinPage } from "./pages/CutinOperationsPages";
 
 import ProductionCreatePage from "./pages/production/ProductionCreatePage";
 import ProductionUpdatePage from "./pages/production/ProductionUpdatePage";
@@ -27,7 +21,6 @@ import ProductionViewPage from "./pages/production/ProductionViewPage";
 import EventUpdatePage from "./pages/event/EventUpdatePage";
 import EventPage from "./pages/event/EventPage";
 import EventCreatePage from "./pages/event/EventCreatePage";
-import EventViewPage from "./pages/event/EventViewPage";
 import ItemCreatePage from "./pages/item/ItemCreatePage";
 import ItemUpdatePage from "./pages/item/ItemUpdatePage";
 import ItemListPage from "./pages/item/ItemListPage";
@@ -52,44 +45,40 @@ export default function App() {
   const guestRoute = (element) => user ? <Navigate to="/produtor" replace /> : element;
   const verifyRoute = (element) => !user ? <Navigate to="/login" replace /> : user.email_verified_at ? <Navigate to="/produtor" replace /> : element;
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<CutinHomePage />} />
-        <Route path="/eventos" element={<EventsHubPage />} />
-        <Route path="/marketplace" element={<MarketplacePage />} />
-        <Route path="/evento/:slug" element={<EventViewPage />} />
+  return <Router><Routes>
+    <Route path="/" element={<CutinHomePage />} />
+    <Route path="/eventos" element={<EventsHubPage />} />
+    <Route path="/marketplace" element={<MarketplacePage />} />
+    <Route path="/evento/:id" element={<PublicEventPage />} />
 
-        <Route path="/login" element={guestRoute(<LoginPage />)} />
-        <Route path="/register" element={guestRoute(<RegisterPage />)} />
-        <Route path="/password-email" element={guestRoute(<PasswordEmailPage />)} />
-        <Route path="/email-verify" element={verifyRoute(<EmailVerifyPage />)} />
-        <Route path="/password" element={protectedRoute(<PasswordPage />)} />
-        <Route path="/logout" element={<LogoutPage />} />
+    <Route path="/login" element={guestRoute(<LoginPage />)} />
+    <Route path="/register" element={guestRoute(<RegisterPage />)} />
+    <Route path="/password-email" element={guestRoute(<PasswordEmailPage />)} />
+    <Route path="/email-verify" element={verifyRoute(<EmailVerifyPage />)} />
+    <Route path="/password" element={protectedRoute(<PasswordPage />)} />
+    <Route path="/logout" element={<LogoutPage />} />
 
-        <Route path="/produtor" element={protectedRoute(<ProducerDashboardPage />)} />
-        <Route path="/dashboard" element={protectedRoute(<DashboardPage />)} />
-        <Route path="/meus-ingressos" element={protectedRoute(<TicketsPage />)} />
-        <Route path="/equipe" element={protectedRoute(<TeamPage />)} />
-        <Route path="/minha-conta" element={protectedRoute(<UserEditPage />)} />
+    <Route path="/produtor" element={protectedRoute(<ProducerDashboardPage />)} />
+    <Route path="/meus-ingressos" element={protectedRoute(<TicketsPage />)} />
+    <Route path="/equipe" element={protectedRoute(<TeamPage />)} />
+    <Route path="/checkin" element={protectedRoute(<CheckinPage />)} />
+    <Route path="/gerenciar/evento/:id" element={protectedRoute(<EventManagePage />)} />
+    <Route path="/minha-conta" element={protectedRoute(<UserEditPage />)} />
 
-        <Route path="/production/create" element={protectedRoute(<ProductionCreatePage />)} />
-        <Route path="/productions" element={protectedRoute(<ProductionPage />)} />
-        <Route path="/production/update/:id" element={protectedRoute(<ProductionUpdatePage />)} />
-        <Route path="/production/:slug" element={protectedRoute(<ProductionViewPage />)} />
+    <Route path="/production/create" element={protectedRoute(<ProductionCreatePage />)} />
+    <Route path="/productions" element={protectedRoute(<ProductionPage />)} />
+    <Route path="/production/update/:id" element={protectedRoute(<ProductionUpdatePage />)} />
+    <Route path="/production/:slug" element={protectedRoute(<ProductionViewPage />)} />
 
-        <Route path="/event" element={protectedRoute(<EventPage />)} />
-        <Route path="/event/create" element={protectedRoute(<EventCreatePage />)} />
-        <Route path="/event/update/:id" element={protectedRoute(<EventUpdatePage />)} />
-        <Route path="/event/:eventId/items" element={protectedRoute(<ItemListPage />)} />
+    <Route path="/event" element={protectedRoute(<EventPage />)} />
+    <Route path="/event/create" element={protectedRoute(<EventCreatePage />)} />
+    <Route path="/event/update/:id" element={protectedRoute(<EventUpdatePage />)} />
+    <Route path="/event/:eventId/items" element={protectedRoute(<ItemListPage />)} />
 
-        <Route path="/item" element={protectedRoute(<ItemListPage />)} />
-        <Route path="/item/create" element={protectedRoute(<ItemCreatePage />)} />
-        <Route path="/item/update/:id" element={protectedRoute(<ItemUpdatePage />)} />
-        <Route path="/item/:id" element={protectedRoute(<ItemViewPage />)} />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-  );
+    <Route path="/item" element={protectedRoute(<ItemListPage />)} />
+    <Route path="/item/create" element={protectedRoute(<ItemCreatePage />)} />
+    <Route path="/item/update/:id" element={protectedRoute(<ItemUpdatePage />)} />
+    <Route path="/item/:id" element={protectedRoute(<ItemViewPage />)} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes></Router>;
 }
