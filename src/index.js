daca59111e4702dc2aae05455035eec693c4beea
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 21:58:15 -0300

    feat: start interaction telemetry for every session

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { startTelemetry } from "./telemetry";
+import { apiBaseUrl, appSlug } from "./config";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
@@ -7,6 +9,8 @@ import "./pages/LegacyPages.css";
 import "./pages/LegacyRasoioOverrides.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+startTelemetry({ apiBaseUrl, appSlug });
 
 const googleClientId = String(process.env.REACT_APP_GOOGLE_CLIENT_ID || "").trim();
 const root = ReactDOM.createRoot(document.getElementById("root"));
