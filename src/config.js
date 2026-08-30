a379a40f835862c541042bbf829d082beb400a5f
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 15:41:33 -0300

    fix: declare Cutinapp application identity

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const apiBaseUrl = "https://api.petertecnet.com.br/api";
 const storageUrl = "https://api.petertecnet.com.br/storage/app/public/";
+const appId = 6;
 const appSlug = "cutinapp";
 const apiV1BaseUrl = `${apiBaseUrl}/v1/apps/${appSlug}`;
 
-export { apiBaseUrl, apiV1BaseUrl, storageUrl, appSlug };
+export { apiBaseUrl, apiV1BaseUrl, storageUrl, appId, appSlug };
