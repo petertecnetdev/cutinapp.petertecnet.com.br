78147505fc011591d624d051da1b00be72c78366
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 15:44:02 -0300

    fix: declare Cutinapp source on every API request

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { startTelemetry } from "./telemetry";
 import { apiBaseUrl, appId, appSlug } from "./config";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import "./App.css";
@@ -11,6 +12,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 startTelemetry({ apiBaseUrl, appSlug, appId });
+
+axios.defaults.headers.common["X-Peter-App"] = appSlug;
+axios.defaults.headers.common["X-App-ID"] = String(appId);
 
 const googleClientId = String(process.env.REACT_APP_GOOGLE_CLIENT_ID || "").trim();
 const root = ReactDOM.createRoot(document.getElementById("root"));
