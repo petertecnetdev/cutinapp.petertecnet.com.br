24d1c4785534e42b688cb876c2751aa91122a231
Peter Tecnet <petertecnet@gmail.com>
2026-08-31 12:04:15 -0300

    fix: use public Laravel storage URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const apiBaseUrl = "https://api.petertecnet.com.br/api";
-const storageUrl = "https://api.petertecnet.com.br/storage/app/public/";
+const storageUrl = "https://api.petertecnet.com.br/storage/";
 const appSlug = "cutinapp";
 const apiV1BaseUrl = `${apiBaseUrl}/v1/apps/${appSlug}`;
 
