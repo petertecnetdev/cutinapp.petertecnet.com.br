df8fdfa4801d5f0265b146edd73929d764dbc1ca
Peter Tecnet <petertecnet@gmail.com>
2026-08-31 19:35:49 -0300

    feat(cutinapp): load social experience styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import "./modern.css";
+import "./social.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import reportWebVitals from "./reportWebVitals";
