0d89824a757ea9f2a2df92704a4bc142e3ef7a19
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 21:31:09 -0300

    feat: add Peter Tecnet signature

---
+++ src/components/PeterTecnetSignature.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PeterTecnetSignature() {
+  return (
+    <footer className="peter-signature" aria-label="Desenvolvido por Peter Tecnet">
+      <span>Desenvolvido por</span>
+      <a href="https://petertecnet.com.br" target="_blank" rel="noreferrer">Peter Tecnet</a>
+    </footer>
+  );
+}
