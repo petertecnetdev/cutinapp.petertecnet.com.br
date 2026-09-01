ed2ced996e177c7527fd627c40321f41f64e8cf1
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:45:34 -0300

    feat: bootstrap Peter Account before Cutinapp

--- src/index.js
+++ src/index.js
@@ -11,14 +11,18 @@ import "./styles/community.css";
 import "./styles/responsive.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import PeterAccountGateway from "./components/PeterAccountGateway";
+import { apiBaseUrl, appSlug } from "./config";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <PeterAccountGateway apiBaseUrl={apiBaseUrl} appSlug={appSlug}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </PeterAccountGateway>
   </React.StrictMode>
 );
 
