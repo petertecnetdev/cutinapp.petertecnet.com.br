445ae330d5c9c17e44d243d64837807eaa41251e
Peter Tecnet <petertecnet@gmail.com>
2026-09-04 01:27:20 -0300

    style: load global error boundary styles

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import "./styles/social-network.css";
 import "./styles/responsive.css";
 import "./styles/logo-theme.css";
 import "./styles/interactive-effects.css";
+import "./styles/error-boundary.css";
 import "./pages/HomeDiscovery.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
