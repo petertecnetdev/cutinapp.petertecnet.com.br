25da5ae7ae661a51337460f7c17601fc6e98b1de
Peter Tecnet <petertecnet@gmail.com>
2026-09-04 13:11:05 -0300

    feat(music): load ambient music styles globally

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "./styles/logo-theme.css";
 import "./styles/interactive-effects.css";
 import "./styles/error-boundary.css";
 import "./pages/HomeDiscovery.css";
+import "./components/ambient/AmbientMusic.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import PeterAccountGateway from "./components/PeterAccountGateway";
