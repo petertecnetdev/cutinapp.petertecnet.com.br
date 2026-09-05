d5468b5889bfc0cc21c774b0fe4fc489373bab5e
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 15:05:35 -0300

    fix(mobile): load canonical responsive layer last

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import "./styles/error-boundary.css";
 import "./pages/HomeDiscovery.css";
 import "./styles/mobile-stability.css";
 import "./styles/peter-navbar-standard.css";
+import "./styles/cutinapp-mobile-final.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import PeterAccountGateway from "./components/PeterAccountGateway";
