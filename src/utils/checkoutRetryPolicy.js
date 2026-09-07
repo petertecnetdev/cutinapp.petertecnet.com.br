e007fcfd26a952b6bc5de7fcc9f9a4fb3a6f37c8
Peter Tecnet <petertecnet@gmail.com>
2026-09-06 21:23:22 -0300

    refactor(checkout): reuse generic idempotency retry policy

--- src/utils/checkoutRetryPolicy.js
+++ src/utils/checkoutRetryPolicy.js
@@ -1,12 +1,3 @@
-import { isNetworkFailure } from "./networkStatus";
+import { shouldKeepIdempotencyAttempt } from "./idempotencyAttempts";
 
-const uncertainStatuses = new Set([408, 409, 425, 429]);
-
-export const shouldKeepCheckoutAttempt = (error) => {
-  if (isNetworkFailure(error)) return true;
-
-  const status = Number(error?.status || error?.response?.status || 0);
-  if (uncertainStatuses.has(status)) return true;
-
-  return status >= 500 && status <= 599;
-};
+export const shouldKeepCheckoutAttempt = shouldKeepIdempotencyAttempt;
