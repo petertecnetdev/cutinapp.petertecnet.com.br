4725e8ab2232d43faa0e78f7368bea40298cf926
Peter Tecnet <petertecnet@gmail.com>
2026-09-09 01:50:56 -0300

    fix(checkout): normalize canceled commerce payments

--- src/services/AppApiClient.js
+++ src/services/AppApiClient.js
@@ -1,6 +1,15 @@
 import { apiV1BaseUrl } from "../config";
+import { normalizeCommercePaymentStatuses } from "../utils/commercePaymentStatus";
 import { createApiClient } from "./ApiClient";
 
 const appApiClient = createApiClient(apiV1BaseUrl);
+
+appApiClient.interceptors.response.use((response) => {
+  const requestUrl = String(response?.config?.url || "");
+  if (requestUrl.includes("/commerce/")) {
+    response.data = normalizeCommercePaymentStatuses(response.data);
+  }
+  return response;
+});
 
 export default appApiClient;
