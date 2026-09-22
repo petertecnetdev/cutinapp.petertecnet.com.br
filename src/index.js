ca4aef8f862ae4630b8e121830a77a284f03cd99
Peter Tecnet <petertecnet@gmail.com>
2026-09-22 13:14:00 -0300

    fix(feed): improve mobile touch targets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/app.css";
+import "./styles/mobileFeedTouchTargets.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import AppErrorBoundary from "./components/AppErrorBoundary";
