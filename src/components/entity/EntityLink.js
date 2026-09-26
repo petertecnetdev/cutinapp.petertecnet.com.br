031d7675ce310afe4ec85b201c168579df200e67
Peter Tecnet <petertecnet@gmail.com>
2026-09-25 22:27:08 -0300

    fix: validate reusable entity link props

--- src/components/entity/EntityLink.js
+++ src/components/entity/EntityLink.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export const entityUrl = (type, entity) => {
@@ -15,3 +16,9 @@ export default function EntityLink({ type, entity, children, ...props }) {
   const url = entity?.url || entityUrl(type || entity?.type, entity);
   return url ? <Link to={url} {...props}>{children || entity?.name || entity?.title}</Link> : <span {...props}>{children || entity?.name || entity?.title}</span>;
 }
+
+EntityLink.propTypes = {
+  type: PropTypes.string,
+  entity: PropTypes.shape({ id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]), slug: PropTypes.string, url: PropTypes.string, type: PropTypes.string, name: PropTypes.string, title: PropTypes.string }),
+  children: PropTypes.node,
+};
