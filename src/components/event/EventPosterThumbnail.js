8862f56f51b74ecc37dc169051255f35bc0a9266
Peter Tecnet <petertecnet@gmail.com>
2026-09-26 18:22:48 -0300

    fix(build): evitar conflito de ordem css

--- src/components/event/EventPosterThumbnail.js
+++ src/components/event/EventPosterThumbnail.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import EventArtwork from "./EventArtwork";
-import "./EventPosterThumbnail.css";
 
 export default function EventPosterThumbnail({
   image,
